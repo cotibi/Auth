8046a957da477dc71aca2d37612f59c933eb5a0d
Tibi <co.tibi@gmail.com>
2021-11-16 23:11:07 +0200

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,35 @@
+import React from "react"
+import Signup from "./Signup"
+import { Container } from "react-bootstrap"
+import { AuthProvider } from "../contexts/AuthContext"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Dashboard from "./Dashboard"
+import Login from "./Login"
+import PrivateRoute from "./PrivateRoute"
+import ForgotPassword from "./ForgotPassword"
+import UpdateProfile from "./UpdateProfile"
+
+function App() {
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <PrivateRoute exact path="/" element={<Dashboard />} />
+              <PrivateRoute path="/update-profile" element={<UpdateProfile />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  )
+}
+
+export default App
